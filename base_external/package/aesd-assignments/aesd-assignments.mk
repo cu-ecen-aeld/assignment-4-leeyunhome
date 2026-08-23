0e75b4aa2ecf5dccd86e764de8edf33efdfcd01c
leeyunhome <mszeta@naver.com>
2026-08-23 15:26:30 +0900

    Install shadow and passwd files in target dir before finalize

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -15,6 +15,28 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/
+	if [ ! -f $(TARGET_DIR)/etc/passwd ]; then \
+		echo "root:x:0:0:root:/root:/bin/sh" > $(TARGET_DIR)/etc/passwd ; \
+		echo "daemon:x:1:1:daemon:/usr/sbin:/bin/sh" >> $(TARGET_DIR)/etc/passwd ; \
+		echo "bin:x:2:2:bin:/bin:/bin/sh" >> $(TARGET_DIR)/etc/passwd ; \
+		echo "default:x:1000:1000:Default user:/home/default:/bin/sh" >> $(TARGET_DIR)/etc/passwd ; \
+	fi
+	if [ ! -f $(TARGET_DIR)/etc/shadow ]; then \
+		echo "root::10933:0:99999:7:::" > $(TARGET_DIR)/etc/shadow ; \
+		echo "daemon:*::0:99999:7:::" >> $(TARGET_DIR)/etc/shadow ; \
+		echo "bin:*::0:99999:7:::" >> $(TARGET_DIR)/etc/shadow ; \
+		echo "default:*::0:99999:7:::" >> $(TARGET_DIR)/etc/shadow ; \
+	fi
+	if [ ! -f $(TARGET_DIR)/etc/group ]; then \
+		echo "root:x:0:" > $(TARGET_DIR)/etc/group ; \
+		echo "daemon:x:1:" >> $(TARGET_DIR)/etc/group ; \
+		echo "bin:x:2:" >> $(TARGET_DIR)/etc/group ; \
+		echo "default:x:1000:" >> $(TARGET_DIR)/etc/group ; \
+	fi
+	if [ ! -f $(TARGET_DIR)/etc/hosts ]; then \
+		echo -e "127.0.0.1\tlocalhost\n127.0.1.1\tbuildroot" > $(TARGET_DIR)/etc/hosts ; \
+	fi
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/ 2>/dev/null || true
